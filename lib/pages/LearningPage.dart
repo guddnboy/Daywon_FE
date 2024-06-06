d508456106e000e6beb30c3a7386508f0bfc44ce
guddnboy <hung3018@naver.com>
2024-06-06 16:55:15 +0900

    학습 페이지 기본 배경

---
+++ lib/pages/LearningPage.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:project/pages/MainPage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: LearningPage(),
+  ));
+}
+
+class LearningPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            double containerWidth = constraints.maxWidth * 0.8;
+            double containerHeight = constraints.maxHeight * 0.65;
+
+            return Stack(
+              children: [
+                Center(
+                  child: Container(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Align(
+                      alignment: const Alignment(
+                          0, 0.3), // Y축 위치 조정 (-1: top, 0: center, 1: bottom)
+                      child: Container(
+                        width: containerWidth,
+                        height: containerHeight,
+                        decoration: ShapeDecoration(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                              width: 2,
+                              color: Color(0xFF4399FF),
+                            ),
+                            borderRadius: BorderRadius.circular(17),
+                          ),
+                          shadows: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/backbtn.png',
+              width: 24,
+              height: 24,
+            ),
+            label: 'Back',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/homebtn.png',
+              width: 28,
+              height: 28,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/mypagebtn.png',
+              width: 24,
+              height: 24,
+            ),
+            label: 'My Page',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          // Handle tap logic here, e.g., navigating to different pages
+          print('Tapped index: $index');
+          switch (index) {
+            case 0:
+              Navigator.pop(context);
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MainPage()),
+              );
+              break;
+            case 2:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MainPage()),
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
