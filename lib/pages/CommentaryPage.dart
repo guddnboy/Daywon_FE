652d6387b462d46b39a07a5a68b4057d86c8293b
guddnboy <hung3018@naver.com>
2024-06-09 22:01:25 +0900

    해설 페이지 기본 레이아웃 추가

---
+++ lib/pages/CommentaryPage.dart
@@ -0,0 +1,337 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:project/pages/MainPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CommentaryPage(),
+    );
+  }
+}
+
+class Problem {
+  final String question;
+  final List<String> choices;
+
+  Problem({required this.question, required this.choices});
+}
+
+Future<Problem> fetchProblem() async {
+  // 데이터베이스나 API 호출로 데이터를 가져오는 부분
+  await Future.delayed(const Duration(seconds: 2)); // 데이터 가져오는 시간 시뮬레이션
+  return Problem(
+    question: '문제 예시가 무언가 있음 이 상황에서 선택해야하는 상품은?',
+    choices: ['보기 1번', '보기 2번', '보기 3번', '보기 4번'],
+  );
+}
+
+class CommentaryPage extends StatelessWidget {
+  final List<Color> buttonColors = [
+    const Color(0xFF8BC0FF),
+    const Color(0xFF55A3FF),
+    const Color(0xFF0075FF),
+    const Color(0xFF0056BD),
+  ];
+
+  CommentaryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<Problem>(
+          future: fetchProblem(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              Problem problem = snapshot.data!;
+              return LayoutBuilder(
+                builder: (context, constraints) {
+                  double containerWidth = constraints.maxWidth * 0.8;
+                  double containerHeight = constraints.maxHeight * 0.65;
+
+                  return Stack(
+                    children: [
+                      Center(
+                        child: Container(
+                          width: constraints.maxWidth,
+                          height: constraints.maxHeight,
+                          clipBehavior: Clip.antiAlias,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                          ),
+                          child: Align(
+                            alignment: const Alignment(0, 0.3),
+                            child: Container(
+                              width: containerWidth,
+                              height: containerHeight,
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  side: const BorderSide(
+                                    width: 2,
+                                    color: Color(0xFF4399FF),
+                                  ),
+                                  borderRadius: BorderRadius.circular(17),
+                                ),
+                                shadows: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.1),
+                                    spreadRadius: 2,
+                                    blurRadius: 10,
+                                    offset: const Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Stack(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20, vertical: 20),
+                                    child: Container(
+                                      width: containerWidth,
+                                      height: containerHeight,
+                                      decoration: ShapeDecoration(
+                                        color: const Color(0xFFBDBDBD),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                      ),
+                                      child: const Center(
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              '정기예금: Point -40',
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16,
+                                                fontFamily: 'Inter',
+                                                fontWeight: FontWeight.w700,
+                                                height: 0,
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              width: 186,
+                                              child: Text.rich(
+                                                TextSpan(
+                                                  children: [
+                                                    TextSpan(
+                                                      text: '정기예금',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w600,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text:
+                                                          '은 일정 기간 동안 돈을 은행에 맡기고 찾지 않는 저축 방식이야.\n',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text: '\n',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text:
+                                                          '이자는 은행이 맡긴 돈에 대해 주는 보상으로, 기간이 길수록 이자도 높아져.\n',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text: '\n',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text:
+                                                          '기간 동안 돈을 찾지 않기로 약속하므로, 약속을 어기면 이자를 덜 받을 수 있어.\n',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text: '\n',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                    TextSpan(
+                                                      text:
+                                                          '1번은 ~~ 해서 답이 될 수 없어\n2번은 ~~ 해서 답이 될 수 없어\n4번은 ~~ 해서 답이 될 수 없어\n',
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                                textAlign: TextAlign.justify,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 60,
+                        right: 50,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Image.asset(
+                            'assets/img/backbtn.png',
+                            width: 45,
+                            height: 45,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 68,
+                        left: 55,
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/img/circle.png',
+                              width: 20,
+                              height: 20,
+                            ),
+                            const SizedBox(width: 5),
+                            const Text(
+                              '오늘의 학습',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            } else {
+              return const Text('No data');
+            }
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/img/backbtn.png',
+              width: 24,
+              height: 24,
+            ),
+            label: 'Back',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/img/homebtn.png',
+              width: 28,
+              height: 28,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/img/mypagebtn.png',
+              width: 24,
+              height: 24,
+            ),
+            label: 'My Page',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pop(context);
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MainPage()),
+              );
+              break;
+            case 2:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MainPage()),
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
