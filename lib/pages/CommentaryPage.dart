ea76860b8bc343e151cdd4edb40cc3499732edba
guddnboy <hung3018@naver.com>
2024-06-09 22:46:22 +0900

    해설 가져오기 함수 추가

--- lib/pages/CommentaryPage.dart
+++ lib/pages/CommentaryPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:project/pages/MainPage.dart';
 import 'package:project/pages/ProblemPage.dart';
@@ -6,8 +8,16 @@ void main() {
   runApp(const MyApp());
 }
 
+// 비동기로 데이터베이스에서 문제 해설 내용을 가져오는 함수
+Future<String> fetchProblemExplanation() async {
+  // 데이터베이스나 API 호출로 데이터를 가져오는 부분
+  await Future.delayed(const Duration(seconds: 1));
+  const commentary = '이곳에 문제에 대한 해설이 기록될 거다.';
+  return commentary;
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +30,10 @@ class MyApp extends StatelessWidget {
 class CommentaryPage extends StatelessWidget {
   final String selectedChoice;
 
-  const CommentaryPage({Key? key, required this.selectedChoice})
-      : super(key: key);
+  const CommentaryPage({super.key, required this.selectedChoice});
 
   @override
   Widget build(BuildContext context) {
-    // 데이터베이스에서 가져온 문제 해설 내용
-    String problemExplanation = '문제에 대한 해설 부분';
-
     return Scaffold(
       body: Center(
         child: LayoutBuilder(
@@ -35,136 +41,153 @@ class CommentaryPage extends StatelessWidget {
             double containerWidth = constraints.maxWidth * 0.8;
             double containerHeight = constraints.maxHeight * 0.65;
 
-            return Stack(
-              children: [
-                Center(
-                  child: Container(
-                    width: constraints.maxWidth,
-                    height: constraints.maxHeight,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                    ),
-                    child: Align(
-                      alignment: const Alignment(0, 0.3),
-                      child: Container(
-                        width: containerWidth,
-                        height: containerHeight,
-                        decoration: ShapeDecoration(
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            side: const BorderSide(
-                              width: 2,
-                              color: Color(0xFF4399FF),
-                            ),
-                            borderRadius: BorderRadius.circular(17),
+            return FutureBuilder<String>(
+              future: fetchProblemExplanation(), // 비동기로 문제 해설 내용을 가져옴
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  String problemExplanation = snapshot.data!;
+                  return Stack(
+                    children: [
+                      Center(
+                        child: Container(
+                          width: constraints.maxWidth,
+                          height: constraints.maxHeight,
+                          clipBehavior: Clip.antiAlias,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
                           ),
-                          shadows: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.1),
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                        ),
-                        child: Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 20),
-                              child: Container(
-                                width: containerWidth,
-                                height: containerHeight,
-                                decoration: ShapeDecoration(
-                                  color: const Color(0xFFBDBDBD),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
+                          child: Align(
+                            alignment: const Alignment(0, 0.3),
+                            child: Container(
+                              width: containerWidth,
+                              height: containerHeight,
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  side: const BorderSide(
+                                    width: 2,
+                                    color: Color(0xFF4399FF),
                                   ),
+                                  borderRadius: BorderRadius.circular(17),
                                 ),
-                                child: Center(
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        selectedChoice, // 선택한 보기 내용 표시
-                                        style: const TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 16,
-                                          fontFamily: 'Inter',
-                                          fontWeight: FontWeight.w700,
-                                          height: 0,
+                                shadows: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.1),
+                                    spreadRadius: 2,
+                                    blurRadius: 10,
+                                    offset: const Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Stack(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 30, vertical: 30),
+                                    child: Container(
+                                      width: containerWidth,
+                                      height: containerHeight,
+                                      decoration: ShapeDecoration(
+                                        color: const Color.fromARGB(
+                                            223, 234, 230, 230),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
                                         ),
                                       ),
-                                      const SizedBox(height: 10),
-                                      SizedBox(
-                                        width: 186,
-                                        child: Text.rich(
-                                          TextSpan(
-                                            children: [
-                                              TextSpan(
-                                                text:
-                                                    problemExplanation, // 문제에 대한 해설 표시
-                                                style: const TextStyle(
-                                                  color: Colors.black,
-                                                  fontSize: 11,
-                                                  fontFamily: 'Inter',
-                                                  fontWeight: FontWeight.w600,
-                                                  height: 0,
-                                                ),
+                                      child: Center(
+                                        child: Column(
+                                          children: [
+                                            Text(
+                                              selectedChoice, // 선택한 보기 내용 표시
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16,
+                                                fontFamily: 'Inter',
+                                                fontWeight: FontWeight.w700,
+                                                height: 0,
                                               ),
-                                            ],
-                                          ),
-                                          textAlign: TextAlign.justify,
+                                            ),
+                                            const SizedBox(height: 10),
+                                            SizedBox(
+                                              width: 186,
+                                              child: Text.rich(
+                                                TextSpan(
+                                                  children: [
+                                                    TextSpan(
+                                                      text:
+                                                          problemExplanation, // 문제에 대한 해설 표시
+                                                      style: const TextStyle(
+                                                        color: Colors.black,
+                                                        fontSize: 11,
+                                                        fontFamily: 'Inter',
+                                                        fontWeight:
+                                                            FontWeight.w600,
+                                                        height: 0,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                                textAlign: TextAlign.justify,
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
-                                    ],
+                                    ),
                                   ),
-                                ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 60,
+                        right: 50,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Image.asset(
+                            'assets/img/backbtn.png',
+                            width: 45,
+                            height: 45,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 68,
+                        left: 55,
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/img/circle.png',
+                              width: 20,
+                              height: 20,
+                            ),
+                            const SizedBox(width: 5),
+                            const Text(
+                              '오늘의 학습',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 60,
-                  right: 50,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Image.asset(
-                      'assets/img/backbtn.png',
-                      width: 45,
-                      height: 45,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 68,
-                  left: 55,
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        'assets/img/circle.png',
-                        width: 20,
-                        height: 20,
-                      ),
-                      const SizedBox(width: 5),
-                      const Text(
-                        '오늘의 학습',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
                     ],
-                  ),
-                ),
-              ],
+                  );
+                } else {
+                  return const Text('No data');
+                }
+              },
             );
           },
         ),
