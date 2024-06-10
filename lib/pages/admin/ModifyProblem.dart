656e3ea65ec9a6234737fdde1a74d5b9e874997b
guddnboy <hung3018@naver.com>
2024-06-11 00:00:35 +0900

    채팅 형식 추가

--- lib/pages/admin/ModifyProblem.dart
+++ lib/pages/admin/ModifyProblem.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:project/pages/MyPage/CorrectProblemDetails.dart';
+import 'package:project/pages/MainPage.dart';
+import 'package:project/pages/MyPage/MyPage.dart';
+import 'package:project/pages/admin/NeedToCheckProblemDetails.dart';
 
 void main() {
   runApp(ModifyProblem());
@@ -7,30 +11,169 @@ void main() {
 class ModifyProblem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 320,
-          height: 568,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Color(0x3F000000),
-                blurRadius: 4,
-                offset: Offset(0, 4),
-                spreadRadius: 0,
-              )
-            ],
+    return Scaffold(
+      body: ChatScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/img/backbtn.png',
+              width: 24,
+              height: 24,
+            ),
+            label: 'Back',
           ),
-          child: Stack(
-            children: [
-              // Place your positioned widgets here...
-            ],
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/img/homebtn.png',
+              width: 28,
+              height: 28,
+            ),
+            label: 'Home',
           ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/img/mypagebtn.png',
+              width: 24,
+              height: 24,
+            ),
+            label: 'My Page',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pop(context);
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MainPage()),
+              );
+              break;
+            case 2:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MyPage()),
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  List<String> messages = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void addMessage(String message) {
+    setState(() {
+      messages.add(message);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        clipBehavior: Clip.antiAlias,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color(0x3F000000),
+              blurRadius: 4,
+              offset: Offset(0, 4),
+              spreadRadius: 0,
+            )
+          ],
         ),
-      ],
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final width = constraints.maxWidth;
+            final height = constraints.maxHeight;
+
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    children: messages
+                        .map((msg) => ChatBubble(message: msg))
+                        .toList(),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _controller,
+                          decoration: const InputDecoration(
+                            hintText: '메시지를 입력하세요...',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        onPressed: () {
+                          final message = _controller.text;
+                          if (message.isNotEmpty) {
+                            addMessage(message);
+                            _controller
+                                .clear(); // Clear the text field after sending the message
+                          }
+                        },
+                        child: const Text('전송'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ChatBubble extends StatelessWidget {
+  final String message;
+
+  ChatBubble({
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Text(
+          message,
+          style: const TextStyle(color: Colors.black),
+        ),
+      ),
     );
   }
 }
