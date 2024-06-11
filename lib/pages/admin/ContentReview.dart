925c2e1215a0bebfbe6788b9974d96bf3cce35aa
guddnboy <hung3018@naver.com>
2024-06-12 01:16:31 +0900

    타이틀 추가

--- lib/pages/admin/ContentReview.dart
+++ lib/pages/admin/ContentReview.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ContentReview());
+  runApp(const MaterialApp(
+    home: ContentReview(),
+  ));
 }
 
 class ContentReview extends StatelessWidget {
   const ContentReview({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
@@ -22,41 +25,63 @@ class ContentReview extends StatelessWidget {
                 blurRadius: 4,
                 offset: Offset(0, 4),
                 spreadRadius: 0,
-              )
+              ),
             ],
           ),
           child: Center(
-            child: Stack(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  decoration: ShapeDecoration(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      side: const BorderSide(
-                        width: 2,
-                        color: Color(0xFF4399FF),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                final width = constraints.maxWidth;
+                final height = constraints.maxHeight;
+                return Stack(
+                  children: [
+                    Container(
+                      width: width * 0.9,
+                      height: height * 0.7,
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                            width: 2,
+                            color: Color(0xFF4399FF),
+                          ),
+                          borderRadius: BorderRadius.circular(17),
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(17),
                     ),
-                  ),
-                ),
-                Positioned(
-                  top: 10,
-                  left: 10,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Image.asset(
-                      'assets/img/backbtn.png',
-                      width: 45,
-                      height: 45,
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      top: height * 0.07,
+                      child: const Center(
+                        child: Text(
+                          '검수할 콘텐츠 선택',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                            height: 1.2,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ],
+                    Positioned(
+                      top: 10,
+                      left: 10,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Image.asset(
+                          'assets/img/backbtn.png',
+                          width: 45,
+                          height: 45,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ),
